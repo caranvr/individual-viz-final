b58251648c46a72b0ef7450a40bc61ac23461d4b
caranvr <cara.navarro.18@ucl.ac.uk>
2021-03-12 14:08:18 +0000

    beautiful

--- website/js/script.js
+++ website/js/script.js
@@ -221,3 +221,16 @@ map.on('load', function(){
 	setData(0);
 
 });
+
+//popup
+	$ = function(id) {
+		return document.getElementById(id);
+	}
+
+	var show = function(id) {
+		$(id).style.display = 'block';
+	}
+
+	var hide = function(id) {
+		$(id).style.display = 'none';
+	}
